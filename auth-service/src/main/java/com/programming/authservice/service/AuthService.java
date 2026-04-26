dd7339567fe1e2da87010796eff19ed1fd8eb9e4
Sayasat <sabitsayasatwork@gmail.com>
2026-04-26 17:26:34 +0500

    added auth/user services, repository, jwt key logic, security config

---
+++ auth-service/src/main/java/com/programming/authservice/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.programming.authservice.service;
+
+import com.programming.authservice.dto.LoginRequestDTO;
+import com.programming.authservice.model.User;
+import com.programming.authservice.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> user = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+        return user;
+    }
+}
